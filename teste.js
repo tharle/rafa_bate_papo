255984e90a44910f0b4a094235f3009b0e99dc9e
tharle <tharlejc@gmail.com>
2025-02-24 18:59:00 -0500

    First commit

---
+++ teste.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+
+	const mensagens = ["Cu cu cu", "Quero me mamar", "Cuceta!"];
+
+	//mensagens.length
+	var i = Math.floor(Math.random() * mensagens.length);
+	console.log(i)
+	res.end(mensagens[i]);
+}).listen(8080);
