5fb1995746228a4b5403be94bb8b6cc8afcee665
Tharle Josefi <tharlejc@gmail.com>
2026-02-10 00:00:32 -0500

    Create teste.js

---
+++ netlify/functions/teste.js
@@ -0,0 +1,10 @@
+exports.handler = async function (event, context) {
+//mensagens.length
+    //var i = Math.floor(Math.random() * mensagens.length)
+
+
+    return {
+      statusCode: 200,
+      body: "Essa é uma mensagem de teste, testando pra ver se vai funcionar o baguho!",
+    };
+  };
